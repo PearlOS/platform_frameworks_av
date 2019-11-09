29c151a9304b9309345715ccf6a64fb475c6afce
darkobas <darkobas@gmail.com>
2019-11-09 21:11:29 +0700

    libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO
    write camera package name if matches
    
    syberia: forward port to 10
    
    Change-Id: I79013ac878f139c2876bb6eb65ca390d099ab26e
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #include <ctime>
 #include <string>
+#ifdef TARGET_NEEDS_CLIENT_INFO
+#include <iostream>
+#include <fstream>
+#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -2486,6 +2490,11 @@ status_t CameraService::BasicClient::startCameraOps() {
 
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
+#ifdef TARGET_NEEDS_CLIENT_INFO
+    std::ofstream cpf("/data/misc/camera/client_package_name");
+    std::string cpn = String8(mClientPackageName).string();
+    cpf << cpn;
+#endif
     return OK;
 }
 
